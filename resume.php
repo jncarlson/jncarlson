b33b3e2208967b7ff3fc74b9b8ad13659268f664
Joseph Carlson <joseph.jn.carlson+github@gmail.com>
2015-07-21 23:27:51 -0600

    Finished Resume Page.

--- resume.php
+++ resume.php
@@ -7,6 +7,7 @@
         <meta name="Joseph Carlson" content="Portfolio & Résumé">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
         <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,400italic' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="style.css">
 
@@ -17,6 +18,49 @@
     <body>
         <div class="container">
             <?php require 'header.php' ?>
-        </div>
+            <h2>Experience</h2>
+            <div id="resume">
+                <h3><strong>2013 - Present:</strong> Freelance Web Developer</h3>
+                <ul>
+                    <li>Custom Wordpress builds</li>
+                    <li>Zend front-end for <a target="_blank" href="https://www.itemexperts.com/">Item Experts</a> CMS</li>
+                </ul>
+                <h3><strong>2014 - Present:</strong> SaaS Web Application Developer, <a target="_blank" href="https://www.simplus.com/">Simplus</a></h3>
+                <ul>
+                    <li>Scrum-based project workfow</li>
+                    <li>Develop apps like <a target="_blank" href="https://www.simplus.com/workfront/products/custom/">Email+, Cast+, Type-Ahead,</a> and <a target="_blank" href="https://www.simplus.com/workfront/products/quickbooks/">Workfront + Quickbooks</a></li>
+                    <li>Dozens of SaaS integrations using Workfront's <a target="_blank" href="https://developers.workfront.com/api-docs/api-explorer/">highly configurable API</a></li>
+                    <li>Custom application builds using Laravel and AngularJS</li>
+                </ul>
+                <h3><strong>2013 - 2014:</strong> Web Infrastructure Developer, <a target="_blank" href="https://www.boostability.com/">Boostability</a></h3>
+                <ul>
+                    <li>Refactored and expanded legacy PHP CMS</li>
+                    <li>Blog wireframe design and creation</li>
+                    <li>Custom Wordpress builds</li>
+                </ul>
+                <h3><strong>2013 - 2013:</strong> Web Developer, <a target="_blank" href="https://www.tinyblessings.com/">Tiny Blessings</a></h3>
+                <ul>
+                    <li>Magento front-end of <a target="_blank" href="https://www.tinyblessings.com/">Tinyblessings.com</a></li>
+                    <li>Created custom menu, scroll, and shopping cart widgets</li>
+                </ul>
+                <h3><strong>2012 - 2013:</strong> Development Internship, <a target="_blank" href="http://superiordesign.com/">Superior Design</a></h3>
+                <ul>
+                    <li>I started out writing HTML and Javascript</li>
+                </ul>
+            </div>
+            <h2>Education</h2>
+            <div id="resume">
+                <ul>
+                    <li>2009: AS <a target="_blank" href="http://www.uvu.edu/csh/">Science</a>, Utah Valley University</li>
+                    <li>expected 2017: BA <a target="_blank" href="https://www.uvu.edu/dgm/pages/programs/internet.html">Web & Application Development</a>, Utah Valley University</li>
+                </ul>
+            </div>
+        </div><!--/.container-->
     </body>
 </html>
+
+<style type="text/css">
+    .p-right li:nth-child(1) {
+        font-weight: bold;
+    }
+</style>
--- use.php
+++ use.php
@@ -7,6 +7,7 @@
         <meta name="Joseph Carlson" content="Portfolio & Résumé">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
         <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,400italic' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="style.css">
 
@@ -20,3 +21,9 @@
         </div>
     </body>
 </html>
+
+<style type="text/css">
+    .p-right li:nth-child(3) {
+        font-weight: bold;
+    }
+</style>
