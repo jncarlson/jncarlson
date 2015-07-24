b8920da1f4e54cd54c7a960b447a4dfccd5dfefd
Joseph Carlson <joseph.jn.carlson+github@gmail.com>
2015-07-23 20:03:18 -0600

    Finished index page.

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
         <title>Joseph Carlson - Software Engineer</title>
         <meta name="Joseph Carlson" content="Portfolio & Résumé">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
         <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,400italic' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="style.css">
@@ -18,6 +18,26 @@
     <body>
         <div class="container">
             <?php require 'header.php' ?>
+            <h2>I'm a web developer.</h2>
+            <div id="resume">
+                <h3>Some things I do...</h3>
+                <ul>
+                    <li>I write HTML, CSS, and client side Javascript. I am familiar with frameworks like <a target="_blank" href="https://angularjs.org/">AngularJS </a>&
+                        <a target="_blank" href="http://getbootstrap.com/">Bootstrap 3</a>.</li>
+                    <li>I also write PHP and design databases in SQL. I program content management systems, APIs, and custom applications. I am also familiar <a target="_blank" href="http://laravel.com/">Laravel</a>.</li>
+                </ul>
+                <h3>Things I don't do...</h3>
+                <ul>
+                    <li>I'm not a designer, I may work <i>with</i> designers.</li>
+                    <li>I am not a sysadmin. Simple web application deployment? Yes. Complex server configuration? No.</li>
+                </ul>
+            </div>
         </div>
     </body>
 </html>
+
+<style type="text/css">
+    h3 {
+        font-size: 1.4em;
+    }
+</style>
--- resume.php
+++ resume.php
@@ -6,7 +6,7 @@
         <title>Joseph Carlson - Résumé</title>
         <meta name="Joseph Carlson" content="Portfolio & Résumé">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
         <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,400italic' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="style.css">
@@ -30,6 +30,7 @@
                     <li>Scrum-based project workfow</li>
                     <li>Develop apps like <a target="_blank" href="https://www.simplus.com/workfront/products/custom/">Email+, Cast+, Type-Ahead,</a> and <a target="_blank" href="https://www.simplus.com/workfront/products/quickbooks/">Workfront + Quickbooks</a></li>
                     <li>Dozens of SaaS integrations using Workfront's <a target="_blank" href="https://developers.workfront.com/api-docs/api-explorer/">highly configurable API</a></li>
+                    <li>Trusted with clients like Nike, DeviantArt, VistaPrint, Xactware, and others</li>
                     <li>Custom application builds using Laravel and AngularJS</li>
                 </ul>
                 <h3><strong>2013 - 2014:</strong> Web Infrastructure Developer, <a target="_blank" href="https://www.boostability.com/">Boostability</a></h3>
