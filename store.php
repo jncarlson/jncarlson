6c38d420a5d87ff253c198eb173792c0700a975e
Joseph Carlson <joseph.jn.carlson+github@gmail.com>
2015-10-12 09:52:22 -0600

    adding stores

---
+++ store.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+
+        <title>Joseph Carlson - Résumé</title>
+        <meta name="Joseph Carlson" content="Portfolio & Résumé">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+        <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,400italic' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="style.css">
+
+        <!--[if lt IE 9]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+    <div class="container">
+        <?php require 'header.php' ?>
+        <form action="" method="POST">
+          <script
+            src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+            data-key="pk_test_tSU0206CYbPPWyqUWW63T7zi"
+            data-amount="2000"
+            data-name="Demo Site"
+            data-description="2 widgets ($20.00)"
+            data-image="/128x128.png"
+            data-locale="auto">
+          </script>
+        </form>
+    </div><!--/.container-->
+    </body>
+</html>
