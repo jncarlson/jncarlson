c3b8e32c1d5722ef0d3cf705055f85f7a91871e4
Joseph Carlson <joseph.jn.carlson+github@gmail.com>
2015-11-01 18:24:20 -0700

    more timesheet

--- cron/alicelane/tsheets/index.php
+++ cron/alicelane/tsheets/index.php
@@ -4,6 +4,8 @@ include_once('tsheets_api/tsheets.inc.php');
 date_default_timezone_set ('America/Denver');
 
 $currentDate = date('Y-m-d');
+$hour_ago = strtotime('-1 day');
+$time = date('Y-m-d', $hour_ago);
 
 $tsheets = new TSheetsRestClient(1, 'S.1__ffdd498faeec5de632b5729eb5164321464327e2');
 //$result = $tsheets->get_report(ReportType::Timesheets,
@@ -11,34 +13,61 @@ $tsheets = new TSheetsRestClient(1, 'S.1__ffdd498faeec5de632b5729eb5164321464327
 //            'end_date' => $currentDate));
 
 
-$timesheet = $tsheets->get(ObjectType::Timesheets, array('start_date' => '2015-2-20',
+$timesheet = $tsheets->get(ObjectType::Timesheets, array('start_date' => $time,
     'end_date' => $currentDate));
 
 $array = $timesheet['results']['timesheets'];
 
 $idsArray = array();
+$jobcodeArray = array();
+
 foreach ($array as $timesheet)
 {
     array_push($idsArray, $timesheet['user_id']);
+    array_push($jobcodeArray, $timesheet['jobcode_id']);
 }
 
 $ids = implode(', ', $idsArray);
+$jobs = implode(', ', $jobcodeArray);
 
 
 $users = $tsheets->get(ObjectType::Users, array('ids' => $ids));
+$jobCodes = $tsheets->get(ObjectType::Jobcodes, array('ids' => $jobs));
+
+
+
 $timesheetArray = array();
 foreach ($array as $timesheet)
 {
 
-//    $user = $tsheets->get(ObjectType::Users, array('ids' => '626832'));
     foreach ($users['results']['users'] as $id)
     {
         if ($id['id'] == $timesheet['user_id'])
         {
             $name = $id['first_name'] . ' ' . $id['last_name'];
+            $group_id = $id['group_id'];
         }
     }
 
+    if ($group_id == "75142")
+    {
+        $group_name = "Designer";
+    }
+    else if ($group_id == "75434")
+    {
+       $group_id = "Administrative";
+    }
+
+    $jobName = "none";
+    foreach ($jobCodes['results']['jobcodes'] as $jobCode)
+    {
+        if ($jobCode['id'] == $timesheet['jobcode_id'])
+        {
+            $jobName = $jobCode['name'];
+        }
+    }
+
+    $timesheet['client_name'] = $jobName;
     $timesheet['user_name'] = $name;
     $timesheet['timesheet_id'] = $timesheet['id'];
     $timesheet['duration'] = gmdate("H:i:s", $timesheet['duration']);
